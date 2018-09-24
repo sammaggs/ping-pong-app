d52946f1d0cc6f6eb4c8c6ba3698dc7d5eecaab6
Sam Maggs <samuelcmaggs@gmail.com>
2018-09-24 11:51:13 +0100

    Header component created

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <Link to="/">
+                <h1>Everyone ❤️'s Ping Pong</h1>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
